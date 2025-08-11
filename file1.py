4dc3a7085db2e189544c4c23d70f9d9515ae996c
vaibhavi26112005 <v9612466@gmail.com>
2025-08-11 19:18:35 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,19 +1,12 @@
+
 try:
-
-    file1=open('sample.txt','r')
-    print("Reading the file content:")
-    reading_file=file1.read()
-    print("Line1:",reading_file)
-    file1.close()
-
-    file1=open('sample.txt','w')
-    writing_file=file1.write('It contains multiple lines\n')
-    file1.close()
-
-    file1=open('sample.txt','r')
-    reading_file=file1.read()
-    print("Line2:",reading_file)
-    file1.close()
+    with open("sample.txt", "r") as file:
+        print("Reading file content:")
+        for i, line in enumerate(file, start=1):
+            print(f"Line {i}: {line.strip()}")
 except FileNotFoundError:
-      print("Error:The file 'sample.txt' was not found ")
+    print("Error: The file 'sample.txt' was not found.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
 
